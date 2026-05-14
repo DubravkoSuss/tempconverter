b148dd1e31c62bd4d0e0f1e459db7cafbe831a3c
root <root@Dudoproton.localdomain>
2026-05-14 20:04:16 +0200

    Fix conftest to use env vars for CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import os
-os.environ['DB_USER'] = 'appuser'
-os.environ['DB_PASS'] = 'apppass'
-os.environ['DB_HOST'] = '10.89.1.7'
-os.environ['DB_NAME'] = 'tempconverter'
+os.environ.setdefault('DB_USER', 'appuser')
+os.environ.setdefault('DB_PASS', 'apppass')
+os.environ.setdefault('DB_HOST', '10.89.1.7')
+os.environ.setdefault('DB_NAME', 'tempconverter')
